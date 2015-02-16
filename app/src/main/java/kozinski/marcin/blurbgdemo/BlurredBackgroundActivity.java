43ba23a0aad6a9f59f927cc80cf4f682ec4fb54c
Marcin Kozinski <marcin.m.kozinski@gmail.com>
2015-02-16 22:25:31 +0100

    Don't squeeze or stretch the images
    I would like to dedicate this commit to Marie Schweiz.

--- app/src/main/java/kozinski/marcin/blurbgdemo/BlurredBackgroundActivity.java
+++ app/src/main/java/kozinski/marcin/blurbgdemo/BlurredBackgroundActivity.java
@@ -4,14 +4,17 @@ import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Picasso.LoadedFrom;
 import com.squareup.picasso.Target;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Display;
 import android.view.View;
 
 import java.util.concurrent.TimeUnit;
@@ -28,16 +31,21 @@ public class BlurredBackgroundActivity extends Activity implements Target {
             "https://raw.githubusercontent.com/MarieSchweiz/lorem-ipsum-illustration/master/png/monster_cyclop_eye_360.png",
             "https://raw.githubusercontent.com/MarieSchweiz/lorem-ipsum-illustration/master/png/monster_suitcase_spider_360.png",
             "https://raw.githubusercontent.com/MarieSchweiz/lorem-ipsum-illustration/master/png/space_monster_360.png"};
+    private static final int BACKGROUND_IMAGES_WIDTH = 360;
+    private static final int BACKGROUND_IMAGES_HEIGHT = 360;
     private static final float BLUR_RADIUS = 25F;
     private final Handler handler = new Handler();
 
     private BlurTransformation blurTransformation;
     private int backgroundIndex;
+    private Point backgroundImageTargetSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         blurTransformation = new BlurTransformation(this, BLUR_RADIUS);
+        backgroundImageTargetSize = calculateBackgroundImageSizeCroppedToScreenAspectRatio(
+                getWindowManager().getDefaultDisplay());
         updateWindowBackground();
     }
 
@@ -64,8 +72,9 @@ public class BlurredBackgroundActivity extends Activity implements Target {
 
     private void updateWindowBackground() {
         String url = getUrlToTheImage();
-        Picasso.with(this).load(url).transform(blurTransformation)
-                .error(R.drawable.background_default).into(this);
+        Picasso.with(this).load(url)
+                .resize(backgroundImageTargetSize.x, backgroundImageTargetSize.y).centerCrop()
+                .transform(blurTransformation).error(R.drawable.background_default).into(this);
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -95,6 +104,27 @@ public class BlurredBackgroundActivity extends Activity implements Target {
         } else {
             //noinspection deprecation
             view.setBackgroundDrawable(drawable);
+        }
+    }
+
+    private static Point calculateBackgroundImageSizeCroppedToScreenAspectRatio(Display display) {
+        final Point screenSize = new Point();
+        getSizeCompat(display, screenSize);
+        int scaledWidth = (int) (((double) BACKGROUND_IMAGES_HEIGHT * screenSize.x) / screenSize.y);
+        int croppedWidth = Math.min(scaledWidth, BACKGROUND_IMAGES_WIDTH);
+        int scaledHeight = (int) (((double) BACKGROUND_IMAGES_WIDTH * screenSize.y) / screenSize.x);
+        int croppedHeight = Math.min(scaledHeight, BACKGROUND_IMAGES_HEIGHT);
+        return new Point(croppedWidth, croppedHeight);
+    }
+
+    @SuppressWarnings("deprecation")
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private static void getSizeCompat(Display display, Point screenSize) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            display.getSize(screenSize);
+        } else {
+            screenSize.x = display.getWidth();
+            screenSize.y = display.getHeight();
         }
     }
 }
